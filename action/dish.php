2fa0e74f80d8c52b12eb43cc6792a2f1d397df5b
KirShet <kirshet2000@gmail.com>
2024-05-15 02:06:58 +0600

    Ensured code functionality across different file locations
    Ensured code functionality across different file locations

--- action/dish.php
+++ action/dish.php
@@ -3,6 +3,6 @@ include '../components/includes.php';
 $dish=$_POST["dish"];
 $selectedDish = getMeal($dish, true);
 $selectedDish['caloriesPerMeal'] = $_POST["caloriesPerMeal"]; 
-$_SESSION['meal'][$value] = $selectedDish;
+$_SESSION['meal'][$dish] = $selectedDish;
 $response['meal'][]=$selectedDish;
 echo json_encode($response);
