094beaad8b7eeff6f969e86124e72202ac8644e5
KirShet <kirshet2000@gmail.com>
2024-05-18 21:03:54 +0600

    'access' session
    The 'access' session variable is checked instead of 'admin'.

--- components/check.php
+++ components/check.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Проверяем, есть ли сессия администратора
-if (!isset($_SESSION['admin']) || $_SESSION['admin'] != 'admin') {
+if (!isset($_SESSION['access']) || $_SESSION['access'] != 'admin') {
     // Если сессии администратора нет, перенаправляем на index.php
     header("Location: index.php");
     exit();
